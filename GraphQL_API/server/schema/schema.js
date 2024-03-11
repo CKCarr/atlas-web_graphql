5c2904fd8c739014720ba14cefe848a974efbca2
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-03-11 10:42:14 -0500

    push 6

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -99,11 +99,25 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args) {
                 return _.find(projects, { id: args.id});
             }
+        },
+        tasks: {
+            type: new GraphQLList(TaskType),
+            resolve(parent, args) {
+                // return all tasks
+                return tasks;
+            }
+        },
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                // return all projects
+                return projects;
+            }
         }
     },
 });
 
 // update schema to include root query
 module.exports = new GraphQLSchema({
-  query: RootQuery,
+    query: RootQuery,
 });
